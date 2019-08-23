f89868cb7b052c555aec6c991d7b2c80c51d2f51
Muhammad Adeel <rana.adeel7273@gmail.com>
2019-08-23 20:41:14 +0500

    Finished
    Java work is done and app is completed

--- EightBall/app/src/main/java/adeelsafdar/eightball/MainActivity.java
+++ EightBall/app/src/main/java/adeelsafdar/eightball/MainActivity.java
@@ -2,7 +2,14 @@ package adeelsafdar.eightball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button askButton;
+        final ImageView eightBall;
+        eightBall = (ImageView) findViewById(R.id.image_eightBall);
+        askButton = (Button) findViewById(R.id.askButton);
+        final int[] ballArray = new int[] {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+                eightBall.setImageResource(ballArray[number]);
+            }
+        });
     }
 }
